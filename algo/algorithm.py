81b812ac5059bfe331fb627b5e424ebf8746bb43
khoowh1996 <74196098+khoowh1996@users.noreply.github.com>
2022-09-17 17:18:51 +0800

    Update algorithm.py to read from firebase storage

--- algo/algorithm.py
+++ algo/algorithm.py
@@ -2,6 +2,25 @@ import pandas as pd
 from math import sqrt
 import csv
 from collections import defaultdict
+import pyrebase
+import urllib
+import io
+
+# Config to read file from firebase storage
+config = {
+"apiKey": "AIzaSyB3EuVdoM4dHQCUwEYScbvbnxiXGXObdnc",
+"authDomain": "fyp-22-s3-07.firebaseapp.com",
+"databaseURL": "https://fyp-22-s3-07-default-rtdb.asia-southeast1.firebasedatabase.app",
+"projectId": "fyp-22-s3-07",
+"storageBucket": "fyp-22-s3-07.appspot.com",
+"serviceAccount": "serviceAccountKey.json"
+}
+
+firebase_storage = pyrebase.initialize_app(config)
+storage = firebase_storage.storage()
+
+url = storage.child("Demo.csv").get_url(None)  # getting the url from storage
+webpage = urllib.request.urlopen(url)
 
 # Sample Dataset
 dataset = {
@@ -40,8 +59,10 @@ dataset = {
 }
 
 # Read the CSV file
-reader = csv.DictReader(open(r'C:\Users\lyhe1\Documents\GitHub\FYP-22-S3-07\algo\Demo.csv'))
+#reader = csv.DictReader(open(r'C:\Users\khoow\OneDrive\Desktop\flask\web1\algo\Demo.csv'))
+reader = csv.DictReader(io.TextIOWrapper(webpage)) #read from the url
 output = defaultdict(dict)
+
 for i in reader:
     output[i['user name']][i['product_name']] = i['rating']
 
