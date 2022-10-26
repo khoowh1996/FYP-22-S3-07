9679ad4cd90471b6369c0ff49969535444ede3ff
khoowh1996 <74196098+khoowh1996@users.noreply.github.com>
2022-10-26 19:57:18 +0800

    Update WebScrapperV2.py

--- algo/WebScrapperV2.py
+++ algo/WebScrapperV2.py
@@ -15,10 +15,11 @@ class ScrapeLazada():
 
     def scrape(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.binary_location = GOOGLE_CHROME_PATH
-        driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
         url = 'https://www.lazada.sg/men-sports-clothing-t-shirts/?from=wangpu'
         #driver = webdriver.Chrome(ChromeDriverManager().install())
         driver.get(url)
